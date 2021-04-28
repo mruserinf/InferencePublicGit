a8e5fac57add5a6592bee74e1a30cf04a5f34b81
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-04-28 16:07:47 +0530

    print.py
    print.py

---
+++ print.py
@@ -0,0 +1,16 @@
+import sys
+import os
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
+print(os.getenv('test'))
+print(os.getenv('test1'))
+
+print('DATA_PATH',os.getenv('DATA_PATH')) 
+print('MODEL_PATH',os.getenv('MODEL_PATH'))
+print('OUTPUT_PATH',os.getenv('OUTPUT_PATH'))
+print('CODE_PATH',os.getenv('CODE_PATH'))
+
+f= open(os.getenv('OUTPUT_PATH') + "/output.csv","w+")
+f.write("1,2,3,4")
+f.close()
