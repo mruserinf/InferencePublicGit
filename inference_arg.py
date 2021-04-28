d89f9a16e9694d3e7e8963ddd6c2b213a4f163a5
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-04-28 19:44:09 +0530

    Update inference_arg.py

--- inference_arg.py
+++ inference_arg.py
@@ -10,12 +10,12 @@ print(os.getenv('test'))
 print(os.getenv('test1'))
 
 print('DATA_PATH',os.getenv('DATA_PATH')) 
-file= os.path.join(os.getenv('DATA_PATH'),'input.csv')
-print ("Is data file exists? " + str(path.isfile('DRS_Model.pl')))
+file1= os.path.join(os.getenv('DATA_PATH'),'input.csv')
+print ("Is data file exists? " + str(path.isfile(file1)))
 
 print('MODEL_PATH',os.getenv('MODEL_PATH'))
-file= os.path.join(os.getenv('MODEL_PATH'),'DRS_Model.pl')
-print ("Is model file exists? " + str(path.isfile('DRS_Model.pl')))
+file2= os.path.join(os.getenv('MODEL_PATH'),'DRS_Model.pl')
+print ("Is model file exists? " + str(path.isfile(file2)))
     
 print('OUTPUT_PATH',os.getenv('OUTPUT_PATH'))
 print('CODE_PATH',os.getenv('CODE_PATH'))
