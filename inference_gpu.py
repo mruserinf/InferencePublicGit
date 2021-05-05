53ef8cfd2868f25aba948ca8be792fcc35793d05
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-05-05 09:14:00 +0530

    Update inference_gpu.py

--- inference_gpu.py
+++ inference_gpu.py
@@ -8,6 +8,7 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
 import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
+print(torch.cuda.get_device_name(0))
 #import tensorflow as tf
 #if tf.test.gpu_device_name():
  #   print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
