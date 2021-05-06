2fcd20d4420eb5696f863d3a50db8ca326d6e635
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-05-06 20:38:55 +0530

    Update inference_arg.py

--- inference_arg.py
+++ inference_arg.py
@@ -1,4 +1,5 @@
 print("Public repo clonned Successfully")
+print("Script Inference_arg.py")
 
 import sys
 import os
@@ -6,8 +7,12 @@ from os import path
 for i in range(len(sys.argv)):
   print(sys.argv[i])
 
-print(os.getenv('env1'))
-print(os.getenv('env2'))
+print(os.getenv('test'))
+print(os.getenv('test1'))
+
+print('DATA_PATH',os.getenv('DATA_PATH')) 
+file1= os.path.join(os.getenv('DATA_PATH'),'input.csv')
+print ("Is data file exists? " + str(path.isfile(file1)))
 
 print('MODEL_PATH',os.getenv('MODEL_PATH'))
 file2= os.path.join(os.getenv('MODEL_PATH'),'DRS_Model.pl')
@@ -19,8 +24,6 @@ print('CODE_PATH',os.getenv('CODE_PATH'))
 f= open(os.getenv('OUTPUT_PATH') + "/output.csv","w+")
 f.write("1,2,3,4")
 f.close()
-
 file3= os.path.join(os.getenv('OUTPUT_PATH'),'output.csv')
 print ("Is output file exists? " + str(path.isfile(file3)))
-    
 print ("output saved in " + os.getenv('OUTPUT_PATH'))
