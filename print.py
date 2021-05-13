96e7069e71a3405ab432644cd23fc5f36169bff3
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-05-13 22:44:21 +0530

    Update print.py

--- print.py
+++ print.py
@@ -3,21 +3,9 @@ import os
 print("Commit2")
 print(sys.argv[1])
 
-print(os.getenv('test'))
-print(os.getenv('test1'))
-
-print('DATA_PATH',os.getenv('DATA_PATH')) 
-print('MODEL_PATH',os.getenv('MODEL_PATH'))
-print('OUTPUT_PATH',os.getenv('OUTPUT_PATH'))
-print('CODE_PATH',os.getenv('CODE_PATH'))
-
-f= open(os.getenv('OUTPUT_PATH') + "/output.csv","w+")
-f.write("1,2,3,4")
-f.close()
-
 import datetime,os,time,sys
 import random
-import string
+
 print("InferencePublicGit master repository")
 print ("Current date and time : ")
 a=[]
