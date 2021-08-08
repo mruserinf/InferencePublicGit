c247a4be483acd1c0f7c55d3164f72c8c5f45148
mruserinf <83266157+mruserinf@users.noreply.github.com>
2021-08-08 16:33:36 +0530

    Create training_arg.py

---
+++ training_arg.py
@@ -0,0 +1,29 @@
+print("Public repo clonned Successfully")
+print("Script training_arg.py")
+
+import sys
+import os
+from os import path
+for i in range(len(sys.argv)):
+  print("arg: " ,sys.argv[i])
+
+print("env1: ", os.getenv('DATA_PATH'))
+print("env2: ", os.getenv('MODEL_PATH'))
+
+print('DATA_PATH',os.getenv('DATA_PATH')) 
+file1= os.path.join(os.getenv('DATA_PATH'),'input.csv')
+print ("Is data file exists? " + str(path.isfile(file1)))
+
+print('MODEL_PATH',os.getenv('MODEL_PATH'))
+file2= os.path.join(os.getenv('MODEL_PATH'),'DRS_Model.pl')
+print ("Is model file exists? " + str(path.isfile(file2)))
+    
+print('OUTPUT_PATH',os.getenv('OUTPUT_PATH'))
+print('CODE_PATH',os.getenv('CODE_PATH'))
+
+f= open(os.getenv('OUTPUT_PATH') + "/output.csv","w+")
+f.write("1,2,3,4")
+f.close()
+file3= os.path.join(os.getenv('OUTPUT_PATH'),'output.csv')
+print ("Is output file exists? " + str(path.isfile(file3)))
+print ("output saved in " + os.getenv('OUTPUT_PATH'))
