03dbf3c7f0e82b68861ce68a550726fd88f270d4
mruserinf <83266157+mruserinf@users.noreply.github.com>
2023-09-07 14:48:47 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,12 +1,10 @@
-print("Public repo clonned Successfully")
-
 import datetime,os,time,sys
 import random
 import string
-print("InferencePublicGit master repository")
+
+print("Public repo with branch Test_Inference_EKS clonned Successfully")
 print ("Current date and time : ")
 a=[]
-
 seconds=int(sys.argv[1])
 i = 1
 while i < seconds:
@@ -20,7 +18,7 @@ while i < seconds:
   i += 1
 
 if __name__ == "__main__":
-
-    with open(os.path.join(os.environ['OUTPUT_PATH'], 'output.csv'), 'w') as output_fd:
-            output_fd.write(str(a))
-            output_fd.write('\n')
+  print("Running inference.py program")
+    # with open(os.path.join(os.environ['OUTPUT_PATH'], 'output.csv'), 'w') as output_fd:
+    #         output_fd.write(str(a))
+    #         output_fd.write('\n')
